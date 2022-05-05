fc7a7e738b7226a527f0bcdbefbf87603feca243
ryu-demons <bapt33333@gmail.com>
2022-05-05 11:24:34 +0200

    ajout de mon fichier python

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 import requests
 
 s = requests.session()
+baptiste = 22
+age_retraite = 62
+
 
 def getAge(name):
     url = "https://api.agify.io?name=" + name
     r = s.get(url).json()
-    print(r.get("age"))
+    age_api = r.get("age")
+    return age_api
 
-getAge("toto")
+
+def diff(real_age):
+    nom = "baptiste"
+    diff_age = getAge(nom) - real_age
+    return diff_age if diff_age > 0 else diff_age * -1
+
+print(diff(baptiste))
